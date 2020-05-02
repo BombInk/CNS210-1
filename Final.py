6e5ec493b39e6d30e2e141138e82fd23ddaae89b
BombInk <62799527+BombInk@users.noreply.github.com>
2020-05-02 12:39:13 -0400

    added content

--- Final.py
+++ Final.py
@@ -7,8 +7,11 @@ from bs4 import BeautifulSoup
 URL = 'https://www.amazon.com/Stonemaier-Games-STM900-Wingspan-Game/' \
       'dp/B07MC9JN56/ref=sr_1_1?dchild=1&keywords=wingspan&qid=1588433425&rnid=2470954011&sr=8-1'
 
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36
+                    (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36'}
 
+page = requests.get(URL, headers=headers)
 
-# is this thing on?
-# mic check
+soup = BeautifulSoup(page.content, 'html.parser')
+
 
